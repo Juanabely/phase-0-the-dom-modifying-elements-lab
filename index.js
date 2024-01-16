b3d99ec1138582e57e018159545db2888fd3e925
Juanabely <abelkipkorir185@gmail.com>
2024-01-16 12:14:09 +0300

    Finished assignment

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ main.remove()
 const newHeader = document.createElement('H1')
 newHeader.id = "victory" 
 document.body.appendChild(newHeader)
-newHeader.innerHTML = "Eugene is the champion"
+newHeader.innerHTML = "Abel is the champion"
