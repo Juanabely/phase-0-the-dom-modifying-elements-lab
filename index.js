c64f63d0a1cbadf9b691d2b9d8361f1c49957328
Juanabely <abelkipkorir185@gmail.com>
2024-01-16 12:12:18 +0300

    Finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const main = document.getElementById("main")
+main.remove()
+const newHeader = document.createElement('H1')
+newHeader.id = "victory" 
+document.body.appendChild(newHeader)
+newHeader.innerHTML = "Eugene is the champion"
